fd775a0dfa2402d65fc1f500062aa21b6c6c8463
SharazFERG <1002429@isd535.org>
2017-03-30 12:51:29 -0500

    Final

--- team13.py
+++ team13.py
@@ -5,18 +5,20 @@
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
-
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+#sharaz and kaan
+team_name = 'Krang' # Only 10 chars displayed.
+strategy_name = 'Mother Brain'
+strategy_description = 'print b if they have a b in their history , print c if they have a c in their history as long as we dont have 3 b's in a row in our history'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
+    if 'b' in their_history:# if they have a b in their history itll print b (example 6's solution)
+        return 'b'
+    elif 'bbb' in my_history:#if we have 3 b's in our history it will return c 
+        return 'c'
+    elif 'c' in their_history:# if they have c in history itll print c (extension of example 6's idea)
+        return 'c'
+    else:
+        return random.choice('cccccccccccccccccccccccccccccccccccccccccccccccccccccbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc')
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
