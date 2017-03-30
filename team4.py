6252223f74473b8a1489ed4d92df2bb4a510e6a9
pietrom54323 <7013302@isd535.org>
2017-03-30 12:58:58 -0500

    Update team4.py

--- team4.py
+++ team4.py
@@ -11,23 +11,28 @@ strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    # shout out to Martin Jones for the inspiration <3
+    if len(my_history) == 0:
+        return 'c'
     
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
+    opp_betray = 0
+    
+    if their_history[-1] == 'b':
+        opp_betray = opp_betray + 1 
+    
+    if len(their_history) < 180:
+        if len(their_history) > 6:
+            if 'b' not in their_history[:7]:
+                 return 'b'
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
+    if opp_betray > 3:
+        return 'b'
+
+   
     return 'c'
-
+        
+    
+        
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
